8cfdee2397d5963fb73fa1f618863aa9ba26d871
Alfonso Alamos <afmalamosa@falabella.cl>
2023-10-11 21:32:40 -0300

    5.- Finalmente en tu actividad o fragmento, crea una instancia de Retrofit y una instancia del servicio web, en este caso ApiService.
    Con estos pasos, tu aplicación Android debería ser capaz de consumir los datos proporcionados por el servicio PHP a través de Retrofit y mostrarlos o procesarlos según sea necesario.

--- app/src/main/java/alamos/alfonso/apirest/MainActivity.kt
+++ app/src/main/java/alamos/alfonso/apirest/MainActivity.kt
@@ -2,10 +2,41 @@ package alamos.alfonso.apirest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val BASE_URL = "https://solucionesdez.com/"; // Dominio o servidor de la API
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+
+        val call = apiService.getUsers()
+
+        call.enqueue(object : Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                if (response.isSuccessful) {
+                    val users = response.body()
+                    // Procesa la lista de usuarios aquí
+                } else {
+                    // Maneja errores de la API aquí
+                }
+            }
+
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                // Maneja errores de red aquí
+            }
+        })
     }
 }
